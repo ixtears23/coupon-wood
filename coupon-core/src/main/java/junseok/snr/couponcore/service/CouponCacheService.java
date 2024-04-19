d599f9f1339c47d7aad1ebf4c3ba414d948857b2
JunseokOh <ixtears23@icloud.com>
2024-04-20 00:51:10 +0900

    Redis 캐시 적용

--- coupon-core/src/main/java/junseok/snr/couponcore/service/CouponCacheService.java
+++ coupon-core/src/main/java/junseok/snr/couponcore/service/CouponCacheService.java
@@ -3,6 +3,7 @@ package junseok.snr.couponcore.service;
 import junseok.snr.couponcore.model.Coupon;
 import junseok.snr.couponcore.repository.redis.dto.CouponRedisEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 public class CouponCacheService {
     private final CouponIssueService couponIssueService;
 
+    @Cacheable(cacheNames = "coupon")
     public CouponRedisEntity getCouponCache(long couponId) {
         final Coupon coupon = couponIssueService.findCoupon(couponId);
         return new CouponRedisEntity(coupon);
