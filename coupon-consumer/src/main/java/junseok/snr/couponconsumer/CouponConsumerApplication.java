0c8e7e22289acd039e7362317153bee808d5ee63
JunseokOh <ixtears23@icloud.com>
2024-04-20 15:45:09 +0900

    Coupon Consumer API 오류 수정

--- coupon-consumer/src/main/java/junseok/snr/couponconsumer/CouponConsumerApplication.java
+++ coupon-consumer/src/main/java/junseok/snr/couponconsumer/CouponConsumerApplication.java
@@ -11,6 +11,6 @@ public class CouponConsumerApplication {
 
     public static void main(String[] args) {
         System.setProperty("spring.config.name", "application-core, application-consumer");
-        SpringApplication.run(CouponCoreConfiguration.class, args);
+        SpringApplication.run(CouponConsumerApplication.class, args);
     }
 }
