3bd716b824e7fb3206c467f870b9a6cc2e816873
JunseokOh <ixtears23@icloud.com>
2024-04-20 15:16:00 +0900

    v2 테스트 설정 추가

---
+++ load-test/locustfile-async-issueV2.py
@@ -0,0 +1,14 @@
+import random
+from locust import task, FastHttpUser
+
+class HelloWorld(FastHttpUser):
+    connection_timeout = 10.0
+    network_timout = 10.0
+
+    @task
+    def issue(self):
+        payload = {
+            "userId": random.randint(1, 10000000),
+            "couponId": 1,
+        }
+        self.client.post("/v2/issue-async", json=payload)
